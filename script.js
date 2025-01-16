1cc14748845b1a3a6a58e512639c22965cd5d577
toril jain <toriljain@gmail.com>
2025-01-16 09:29:27 +0530

    changes

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const submitButton=document.getElementById("calculate")
+// const submitButton=document.getElementById("calculate")
 
-function calculate(){
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-    let operator = document.getElementById("operation").value;
+// function calculate(){
+//     let num1 = parseFloat(document.getElementById("num1").value);
+//     let num2 = parseFloat(document.getElementById("num2").value);
+//     let operator = document.getElementById("operation").value;
 
-    if (operator=="add"){
-        res= num1 + num2;
-    }
-    else if (operator=="sub"){
-        res= num1 - num2;
-    }
-    else if (operator=="mul"){
-        res= num1 * num2;
-    }
-    else if (operator=="div"){
-        if (num2==0){
-            alert("Error: Division by zero is not allowed");
-        }
-        else{
-            res= num1 / num2;
-        }
-    }
-    alert("the answer is " + res)
+//     if (operator=="add"){
+//         res= num1 + num2;
+//         alert("the answer is " + res)
+//     }
+//     else if (operator=="sub"){
+//         res= num1 - num2;
+//         alert("the answer is " + res)
+//     }
+//     else if (operator=="mul"){
+//         res= num1 * num2;
+//         alert("the answer is " + res);
+//     }
+//     else if (operator=="div"){
+//         if (num2==0){
+//             alert("Error: Division by zero is not allowed");
+//         }
+//         else{
+//             res= num1 / num2;
+//             alert("the answer is " + res)
+//         }
+//     }
     
-}
+    
+// }
 
 submitButton.addEventListener("click",calculate())
